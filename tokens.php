480d32b169ed042b27d1dda3a18cdaf419264516
champ <analycer@gmail.com>
2018-05-31 14:53:14 +0700

    Create tokens.php

---
+++ tokens.php
@@ -0,0 +1,7 @@
+<?PHP
+
+$token_access = 'QL1eDf9RyBtwa/OgMx/h/ZrMeYIEPcZpqHeFHjJ3youZCtPNup1sCmD3l8NI2Mo7ZUQJve4EFPoGXFcJ3qIi8HXN51tYRLvbGkLLCYWtM6JZSTk9ldiyof/5fA3F2CGZOVHODTzfjKrYVbkeYI4gIgdB04t89/1O/w1cDnyilFU=';
+
+$token_channel = 'a4a9a5b45e89af6ee640b5be50cc0090';
+
+$token_id = 'Ua0c0cd1ee5061638a0264e9b12041b78';
