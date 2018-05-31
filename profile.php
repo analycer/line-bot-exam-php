9603dfbf21e7ace00e0e4d9137c3c8f714fa732b
champ <analycer@gmail.com>
2018-05-31 14:56:59 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,11 @@
 <?php
 
+require ("tokens.php");
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$access_token = $token_access;
+
+$userId = $token_id;
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
