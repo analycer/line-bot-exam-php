17e22e93178c6ad860924454531f7ca759dea023
Champ <champ@Champ-MBA.local>
2018-06-04 12:30:05 +0700

    test

--- botpush.php
+++ botpush.php
@@ -1,36 +1,35 @@
 <?php
 
 require "tokens.php";
-
 require "vendor/autoload.php";
 
 $access_token = $token_access;
-
 $channelSecret = $token_channel;
-
 $pushID = $token_id; // user id
 
+$user_ids = array('Ua0c0cd1ee5061638a0264e9b12041b78','Ua0c0cd1ee5061638a0264e9b12041b78'); // users to send msg to
+
+$msg = 'hello world';
+if (!empty($_GET["msg"])) $msg = $_GET["msg"]; 
+
+// Construct CLIENT / BOT / MESSAGE
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
+// Send to all users
+foreach ($user_ids as $id) 
+    $response = $bot->pushMessage($id, $textMessageBuilder);
 
-echo "<pre>";
-var_dump( $httpClient );
-echo "<hr>";
-var_dump( $bot );
-echo "<hr>";
-var_dump( $textMessageBuilder );
-echo "<hr>";
-var_dump( $response );
-echo "<hr>";
+
+// echo "<pre>";
+// var_dump( $httpClient );
+// echo "<hr>";
+// var_dump( $bot );
+// echo "<hr>";
+// var_dump( $textMessageBuilder );
+// echo "<hr>";
+// var_dump( $response );
+// echo "<hr>";
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-
-
-
-
-
-
