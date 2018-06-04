dcf6e8385fd7620bc6d1a42de18dc9f3d1c726cb
Champ <champ@Champ-MBA.local>
2018-06-04 13:10:58 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -38,7 +38,8 @@ if (!is_null($events['events'])) {
 					$outbound_message .= '99.99%: '.$current_price['g99lbma_bid']." - ".$current_price['g99lbma_ask']."\n";
 				
 			} else {
-				$outbound_message = $event['source']['userId'];     
+				//$outbound_message = $event['source']['userId'];
+				$outbound_message = var_dump($event);
 			}
 			
 			// Get replyToken
