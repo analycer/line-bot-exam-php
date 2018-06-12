d48b5363b7cdcc506d50bacc49ba3d1683d934f2
Champ <champ@Champ-MBA.local>
2018-06-12 17:56:09 +0700

    test

---
+++ notify_trader.php
@@ -0,0 +1,37 @@
+<?php
+
+require "tokens.php"; 
+require "vendor/autoload.php";
+
+$access_token = $token_access;
+$channelSecret = $token_channel;
+$pushID = $token_id; // user id
+
+$trader_line_group_id = 'C776346348c880cb97c95f4580ea7e8da';
+
+
+// ------------------------- DEFAULTS / FORM ---------------------------------------
+$msg = '';
+if (!empty($_GET["msg"])) $msg = $_GET["msg"]; 
+
+
+// -------------------------- Construct CLIENT / BOT / MESSAGE -----------------------
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+
+// ----------------------------- Send to all users -----------------------------------
+//foreach ($user_ids as $id) 
+$response = $bot->pushMessage($trader_line_group_id, $textMessageBuilder);
+
+
+//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+if ($response->getHTTPStatus() == 200) 
+    echo "<font color='ForestGreen'>ส่งข้อความแล้ว</font><br>$msg";
+else {
+    echo "<font color='red'>ไม่สามารถส่งข้อความได้ </font><hr><pre>";
+    // var_dump( $httpClient );
+    // var_dump( $bot );
+    var_dump( $textMessageBuilder ); echo "<hr>";
+    var_dump( $response );
+}
